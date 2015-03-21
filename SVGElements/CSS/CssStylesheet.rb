e8b96d16d03fdc2dcc96a93218f6d5595ac1cca0
Robert Palmer <robisacommonusername@server.fake>
2015-03-21 11:19:31 +0800

    Added support for the <style> tag and inline stylesheets

---
+++ SVGElements/CSS/CssStylesheet.rb
@@ -0,0 +1,17 @@
+require_relative 'CssClass'
+class CssStylesheet
+	def initialize
+		@classes = []
+	end
+	
+	def add_class(name)
+		c = CssClass.new(name)
+		if block_given? yield c end
+		@classes << c
+		return c
+	end
+	
+	def to_css
+		@classes.map{|c| c.to_css}.join("\n")
+	end
+end
