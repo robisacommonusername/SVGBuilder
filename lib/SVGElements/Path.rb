cdc941a9a4da352bde84a4ff777b376f6aa1df2f
Robert Palmer <robisacommonusername@server.fake>
2015-03-26 18:34:43 +0800

    Added path parsing to prevent illegal path operations being given in path string, and for compatability with RVG (rvg allows slightly non-standard path specifications, but renders them correctly)

--- lib/SVGElements/Path.rb
+++ lib/SVGElements/Path.rb
@@ -4,7 +4,7 @@ class SVG < SVGAbstract::SVGContainer
 		def initialize(d)
 			super()
 			@name = 'path'
-			@attributes[:d] = d
+			@attributes[:d] = parse_path d
 			
 			yield self if block_given?
 			return self
@@ -15,9 +15,9 @@ class SVG < SVGAbstract::SVGContainer
 		#e.g. you can draw a triangle like this:
 		#self.M(0,0).L(0,1).L(1,0).Z
 		#Also available: m, l, H, h, V, v, C, c, S, s, Q, q, T, t, A, a
-		command_arities = {:M=>2,:L=>2,:H=>1,:V=>1,:C=>[2,2,2],:S=>[2,2],
+		@@command_arities = {:M=>2,:L=>2,:H=>1,:V=>1,:C=>[2,2,2],:S=>[2,2],
 			:Q=>[2,2], :T=>2, :A=>7, :Z=>0}
-		command_arities.each do |k,v|
+		@@command_arities.each do |k,v|
 			unless v.is_a? Array
 				v = [v]
 			end
@@ -48,5 +48,32 @@ class SVG < SVGAbstract::SVGContainer
 		alias_method :cubic_bezier, :C
 		alias_method :quadratic_bezier, :Q
 		alias_method :arc, :A
+		
+		#RVG allows paths to be specified that aren't actually quite
+		#standard, but manages to interpret them. Thus we'll do that too
+		def parse_path(d)
+			parsed = ''
+			while (off = (d =~ /[A-Za-z]/))
+				command = d[off]
+				d.slice!(0..off)
+				if @@command_arities.has_key? command.upcase.to_sym
+					arity = @@command_arities[command.upcase.to_sym]
+					arity = [arity] unless arity.is_a? Array
+					args = arity.map do |n|
+						nums = []
+						n.times do |i|
+							off = (d =~ /[-0-9]+/)
+							off += ($~[0].length-1)
+							d.slice!(0..off)
+							nums << $~[0]
+						end
+						nums
+					end
+					parsed += " #{command} #{args.map{|s| s.join(' ')}.join(', ')}"
+				end
+			end
+			return parsed.slice(1..-1) #cut off initial space
+		end
+		private :parse_path
 	end
 end
