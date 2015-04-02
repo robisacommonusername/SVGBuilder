7b79e0b442e6d25e2628d1f46319d918608fe184
Robert Palmer <robisacommonusername@server.fake>
2015-04-02 16:54:47 +0800

    Added escaping for cdata

--- lib/SVGElements/Style.rb
+++ lib/SVGElements/Style.rb
@@ -26,10 +26,16 @@ class SVG < SVGAbstract::SVGContainer
 			return self
 		end
 		
+		def escape_cdata(cdata)
+			#remove ]]>
+			cdata.gsub(/\]\s*\]\s*\>/, '')
+		end
+		private :escape_cdata
+		
 		def to_xml
 			#need to place css from css and stylesheet into cdata
 			xml = "<style>\n"
-			xml += "/* <![CDATA[ */\n#{@css}\n#{@stylesheet.to_css}\n /* ]]> */"
+			xml += "/* <![CDATA[ */\n#{escape_cdata @css}\n#{escape_cdata @stylesheet.to_css}\n /* ]]> */"
 			xml += @svg_objects.map{|o| o.to_xml}.join("\n")
 			xml += "\n</style>"
 		end
